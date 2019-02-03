d5db1f57e1d809253b40372b4188ac29eb41160b
Dooley_labs <elderlabs@users.noreply.github.com>
2019-02-03 16:42:05 -0500

    Update client.py

--- disco/gateway/client.py
+++ disco/gateway/client.py
@@ -3,6 +3,8 @@ import zlib
 import six
 import ssl
 
+from websocket import ABNF
+
 from disco.gateway.packets import OPCode, RECV, SEND
 from disco.gateway.events import GatewayEvent
 from disco.gateway.encoding import ENCODERS
@@ -11,16 +13,18 @@ from disco.util.logging import LoggingClass
 from disco.util.limiter import SimpleLimiter
 
 TEN_MEGABYTES = 10490000
+ZLIB_SUFFIX = b'\x00\x00\xff\xff'
 
 
 class GatewayClient(LoggingClass):
     GATEWAY_VERSION = 6
 
-    def __init__(self, client, max_reconnects=5, encoder='json', ipc=None):
+    def __init__(self, client, max_reconnects=5, encoder='json', zlib_stream_enabled=True, ipc=None):
         super(GatewayClient, self).__init__()
         self.client = client
         self.max_reconnects = max_reconnects
         self.encoder = ENCODERS[encoder]
+        self.zlib_stream_enabled = zlib_stream_enabled
 
         self.events = client.events
         self.packets = client.packets
@@ -36,6 +40,7 @@ class GatewayClient(LoggingClass):
         # Create emitter and bind to gateway payloads
         self.packets.on((RECV, OPCode.DISPATCH), self.handle_dispatch)
         self.packets.on((RECV, OPCode.HEARTBEAT), self.handle_heartbeat)
+        self.packets.on((RECV, OPCode.HEARTBEAT_ACK), self.handle_heartbeat_acknowledge)
         self.packets.on((RECV, OPCode.RECONNECT), self.handle_reconnect)
         self.packets.on((RECV, OPCode.INVALID_SESSION), self.handle_invalid_session)
         self.packets.on((RECV, OPCode.HELLO), self.handle_hello)
@@ -47,6 +52,8 @@ class GatewayClient(LoggingClass):
         # Websocket connection
         self.ws = None
         self.ws_event = gevent.event.Event()
+        self._zlib = None
+        self._buffer = None
 
         # State
         self.seq = 0
@@ -61,6 +68,7 @@ class GatewayClient(LoggingClass):
 
         # Heartbeat
         self._heartbeat_task = None
+        self._heartbeat_acknowledged = True
 
     def send(self, op, data):
         self.limiter.check()
@@ -76,7 +84,14 @@ class GatewayClient(LoggingClass):
 
     def heartbeat_task(self, interval):
         while True:
+            if not self._heartbeat_acknowledged:
+                self.log.warning('Received HEARTBEAT without HEARTBEAT_ACK, forcing a fresh reconnect')
+                self._heartbeat_acknowledged = True
+                self.ws.close(status=4000)
+                return
+
             self._send(OPCode.HEARTBEAT, self.seq)
+            self._heartbeat_acknowledged = False
             gevent.sleep(interval / 1000)
 
     def handle_dispatch(self, packet):
@@ -89,22 +104,26 @@ class GatewayClient(LoggingClass):
     def handle_heartbeat(self, _):
         self._send(OPCode.HEARTBEAT, self.seq)
 
+    def handle_heartbeat_acknowledge(self, _):
+        self.log.debug('Received HEARTBEAT_ACK')
+        self._heartbeat_acknowledged = True
+
     def handle_reconnect(self, _):
         self.log.warning('Received RECONNECT request, forcing a fresh reconnect')
         self.session_id = None
         self.ws.close()
 
     def handle_invalid_session(self, _):
-        self.log.warning('Recieved INVALID_SESSION, forcing a fresh reconnect')
+        self.log.warning('Received INVALID_SESSION, forcing a fresh reconnect')
         self.session_id = None
         self.ws.close()
 
     def handle_hello(self, packet):
-        self.log.info('Recieved HELLO, starting heartbeater...')
+        self.log.info('Received HELLO, starting heartbeater...')
         self._heartbeat_task = gevent.spawn(self.heartbeat_task, packet['d']['heartbeat_interval'])
 
     def on_ready(self, ready):
-        self.log.info('Recieved READY')
+        self.log.info('Received READY')
         self.session_id = ready.session_id
         self.reconnects = 0
 
@@ -122,6 +141,9 @@ class GatewayClient(LoggingClass):
 
         gateway_url += '?v={}&encoding={}'.format(self.GATEWAY_VERSION, self.encoder.TYPE)
 
+        if self.zlib_stream_enabled:
+            gateway_url += '&compress=zlib-stream'
+
         self.log.info('Opening websocket connection to URL `%s`', gateway_url)
         self.ws = Websocket(gateway_url)
         self.ws.emitter.on('on_open', self.on_open)
@@ -129,17 +151,35 @@ class GatewayClient(LoggingClass):
         self.ws.emitter.on('on_close', self.on_close)
         self.ws.emitter.on('on_message', self.on_message)
 
-        self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+        self.ws.run_forever(sslopt={'cert_reqs': ssl.CERT_NONE})
 
     def on_message(self, msg):
-        # Detect zlib and decompress
-        is_erlpack = ((six.PY2 and ord(msg[0]) == 131) or (six.PY3 and msg[0] == 131))
-        if msg[0] != '{' and not is_erlpack:
-            msg = zlib.decompress(msg, 15, TEN_MEGABYTES).decode("utf-8")
+        if self.zlib_stream_enabled:
+            if not self._buffer:
+                self._buffer = bytearray()
+
+            self._buffer.extend(msg)
+
+            if len(msg) < 4:
+                return
+
+            if msg[-4:] != ZLIB_SUFFIX:
+                return
+
+            msg = self._zlib.decompress(self._buffer if six.PY3 else str(self._buffer))
+            # If this encoder is text based, we want to decode the data as utf8
+            if self.encoder.OPCODE == ABNF.OPCODE_TEXT:
+                msg = msg.decode('utf-8')
+            self._buffer = None
+        else:
+            # Detect zlib and decompress
+            is_erlpack = ((six.PY2 and ord(msg[0]) == 131) or (six.PY3 and msg[0] == 131))
+            if msg[0] != '{' and not is_erlpack:
+                msg = zlib.decompress(msg, 15, TEN_MEGABYTES).decode('utf-8')
 
         try:
             data = self.encoder.decode(msg)
-        except:
+        except Exception:
             self.log.exception('Failed to parse gateway message: ')
             return
 
@@ -157,13 +197,16 @@ class GatewayClient(LoggingClass):
         raise Exception('WS recieved error: %s', error)
 
     def on_open(self):
+        if self.zlib_stream_enabled:
+            self._zlib = zlib.decompressobj()
+
         if self.seq and self.session_id:
             self.log.info('WS Opened: attempting resume w/ SID: %s SEQ: %s', self.session_id, self.seq)
             self.replaying = True
             self.send(OPCode.RESUME, {
                 'token': self.client.config.token,
                 'session_id': self.session_id,
-                'seq': self.seq
+                'seq': self.seq,
             })
         else:
             self.log.info('WS Opened: sending identify payload')
@@ -180,10 +223,13 @@ class GatewayClient(LoggingClass):
                     '$browser': 'disco',
                     '$device': 'disco',
                     '$referrer': '',
-                }
+                },
             })
 
     def on_close(self, code, reason):
+        # Make sure we cleanup any old data
+        self._buffer = None
+
         # Kill heartbeater, a reconnect/resume will trigger a HELLO which will
         #  respawn it
         if self._heartbeat_task:
@@ -217,3 +263,15 @@ class GatewayClient(LoggingClass):
     def run(self):
         gevent.spawn(self.connect_and_run)
         self.ws_event.wait()
+
+    def request_guild_members(self, guild_id_or_ids, query=None, limit=0):
+        """
+        Request a batch of Guild members from Discord. Generally this function
+        can be called when initially loading Guilds to fill the local member state.
+        """
+        self.send(OPCode.REQUEST_GUILD_MEMBERS, {
+            # This is simply unfortunate naming on the part of Discord...
+            'guild_id': guild_id_or_ids,
+            'query': query or '',
+            'limit': limit,
+        })
