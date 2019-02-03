2a8d356e330e99dbb460310f95ed26586054adfd
Dooley_labs <elderlabs@users.noreply.github.com>
2019-02-03 17:11:23 -0500

    Update base.py
    revert if breaking

--- tests/types/base.py
+++ tests/types/base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from disco.types.base import Model, Field, cached_property
+from disco.types.base import Model, Field, cached_property, text
 
 
 @pytest.fixture
@@ -29,3 +29,24 @@ def test_cached_property_clear_on_update(model):
     assert inst.value == 4
     inst.inplace_update(model(a=2, b=3))
     assert inst.value == 5
+
+
+def test_defaults():
+    class TestModel(Model):
+        a = Field(int, default=None)
+        b = Field(int, default=0)
+
+    model = TestModel()
+    assert model.a is None
+    assert model.b == 0
+
+
+def test_text_casting():
+    class TestModel(Model):
+        a = Field(text)
+
+    model = TestModel({'a': 1})
+    assert model.a == '1'
+
+    model = TestModel({'a': {}})
+    assert model.a == '{}'
